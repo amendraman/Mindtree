dfe10a79c999b93ea9e8c1a71a5d62f9f8ee8d17
rocky <amansingh0624@gmail.com>
2017-12-22 08:47:47 +0530

    HibernateSpring

---
+++ HibernateSpring/src/com/gdg/rocky/pojo/OrchardPojo.java
@@ -0,0 +1,43 @@
+package com.gdg.rocky.pojo;
+
+public class OrchardPojo {
+	
+	private String mid;
+	private String name;
+	private String email;
+	private String password;
+	private int phone;
+	public String getMid() {
+		return mid;
+	}
+	public void setMid(String mid) {
+		this.mid = mid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	
+	
+
+}
