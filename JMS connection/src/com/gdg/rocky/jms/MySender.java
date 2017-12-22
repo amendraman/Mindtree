7772668480e35614039ad9084b111cd82a663cde
rocky <amansingh0624@gmail.com>
2017-12-22 08:51:41 +0530

    JMSConnection

---
+++ JMS connection/src/com/gdg/rocky/jms/MySender.java
@@ -0,0 +1,24 @@
+package com.gdg.rocky.jms;
+
+import javax.naming.InitialContext;
+import javax.print.attribute.standard.QueuedJobCount;
+import java.io.BufferedReader;  
+import java.io.InputStreamReader;  
+import javax.naming.*;  
+import javax.jms.*;
+
+public class MySender {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try{
+			InitialContext ctx = new InitialContext();
+			QueueConnectionFac
+		}
+		catch(Exception e)
+		{
+			
+		}
+	}
+
+}
